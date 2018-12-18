c1d56f5354890b01a65ab87a137275989678e380
Roman Dvoskin <rdvoskin@circle.com>
2018-12-18 09:59:00 -0500

    works

--- daemon/daemon.php
+++ daemon/daemon.php
@@ -1,20 +1,39 @@
 <?php
 function run() {
+	logMessage("Opening connection");
 	$conn = mysqli_connect('db', 'user', 'test', "myDb");
-    while (true) {
-        printData($conn);
-		sleep(1000);
+	logMessage("Opened connection");
+	while (true) {
+		try {
+			printData($conn);
+		} catch (Exception $e) {
+			logMessage($e->getMessage());
+		}
+		sleep(5);
 	}
 }
 
 function printData($conn){
 	$query = 'SELECT * FROM Person';
 	$result = mysqli_query($conn, $query);
+	if (!$result) {
+		logMessage("Connection error");
+		return;
+	}
 	while ($value = $result->fetch_array(MYSQLI_ASSOC)) {
+		logMessage("Selected data");
 		foreach ($value as $element) {
-			print $element;
+			logMessage($element);
 		}
 	}
+}
+
+function logMessage($message) {
+	$message = $message . "/n";
+	print $message;
+	$log = fopen("/var/log/roman/daemon.log", "w") or die("Unable to open log file!");
+	fwrite($log, $message);
+	fclose($log);
 }
 
 run();
