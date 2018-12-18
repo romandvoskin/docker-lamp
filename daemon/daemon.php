0dae2c1ebf5569a162fa5b432602f753c231f58a
Roman Dvoskin <rdvoskin@circle.com>
2018-12-17 22:58:14 -0500

    Reuse conn

--- daemon/daemon.php
+++ daemon/daemon.php
@@ -1,13 +1,13 @@
 <?php
 function run() {
-	while (true) {
-        printData();
+	$conn = mysqli_connect('db', 'user', 'test', "myDb");
+    while (true) {
+        printData($conn);
 		sleep(1000);
 	}
 }
 
-function printData(){
-	$conn = mysqli_connect('db', 'user', 'test', "myDb");
+function printData($conn){
 	$query = 'SELECT * From Person';
 	$result = mysqli_query($conn, $query);
 	while ($value = $result->fetch_array(MYSQLI_ASSOC)) {
